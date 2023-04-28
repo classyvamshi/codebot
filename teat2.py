aa6fdf18c5984c56bfbeb975602fd048cf1a4176
classyvamshi <121681236+classyvamshi@users.noreply.github.com>
2023-04-28 11:50:04 +0530

    Create teat2.py

---
+++ teat2.py
@@ -0,0 +1,25 @@
+!pip install openai
+import openai
+openai.api_key = "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"
+
+params = {}
+
+params["temperature"] = 0.7
+params["max_tokens"]=256
+params["model"] = "text-davinci-003"
+
+prompt = "Why is water conservation important?"
+
+def llm(prompt):
+
+  res = openai.Completion.create(
+      engine = params["model"],
+      prompt = prompt,
+      temperature = params["temperature"],
+      max_tokens = params["max_tokens"],
+  )
+
+  return res.choices[0].text
+  
+  llm("Why is water conservation important? ")
+  
